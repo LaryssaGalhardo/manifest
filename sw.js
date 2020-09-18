4ca765f051b88224710c34b10a32cfec19944b7c
LaryssaGalhardo <65986329+LaryssaGalhardo@users.noreply.github.com>
2020-09-17 21:05:00 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+//Declarar as variaveis / constantes necesarias para atuacao do nosso service worker
+var CACHE_NAME = 'fofo-cache-v1'
+var urlsToCache = [
+    'css/fofo.css',
+    'css/bootstrap.css',
+    'js/bootstrap.js',
+    'js/jquery-3.4.1.js',
+]
+
+self.addEventListener('install', function(event){
+    //Paremetrizar as etapas de instalacao do nosso cache no dispositivo
+    event.wait(
+        caches.open(CACHE_NAME)
+        .then(function(cache){
+            console.log('Cache aberto...')
+            return cache.addAll(urlsToCache)
+        })
+    )
+
+})
+
+self.addEventListener('fetch', function(event){
+     event.respondWith(
+         caches.match(event.request)
+         .then(function(response){
+             if(response) {
+                 return response
+             }
+             return fetch(event.request)
+         })
+     )
+})
